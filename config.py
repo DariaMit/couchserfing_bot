4289775dcb9e5247d7a15818502600080b722f36
DariaMit <103268368+DariaMit@users.noreply.github.com>
2022-05-23 16:03:35 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-TG_TOKEN = '5163013363:AAE9P2UGWiLwt7RNtHZjJZSg_Ubr9GZPTXU'
-
-db_user = 'root'
-db_password = 'mt4613rfn'
-
-vk_user_token = '6431840bdf550b7d7bd707fb1ca2ea845cc83e18f5ca1e488ffda49545b5c88af981d9d34376b3c0af729'
